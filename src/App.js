f61d8bb0713741a6c4f450a119f9a19acd56fe23
john feyisayo <johnfeyisayo@gmail.com>
2023-06-14 17:58:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import AdviceCard from "./components/AdviceCard";
+
+function App() {
+  return (
+    <div className="App">
+    <AdviceCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AdviceCard.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import dividerDesktop from "./images/pattern-divider-desktop.svg";
+import dividerMobile from "./images/pattern-divider-mobile.svg";
+import dice from "./images/icon-dice.svg";
+
+function AdviceCard() {
+  const [advice, setAdvice] = useState('');
+
+  const getAdvice = async() => {
+    const response = await axios.get('https://api.adviceslip.com/advice')
+    const advise = await response.data.slip
+    setAdvice(advise)
+  }
+
+  useEffect(() => {
+    getAdvice();
+  },[])
+   return (
+    <div className="card">
+      <p>ADVICE #{advice.id}</p>
+      <h2>{advice.advice}</h2>
+      <img src={dividerDesktop} className="divider-desktop" alt="" />
+      <img src={dividerMobile} className="divider-mobile" alt="" />
+      <div className="dice">
+        <img src={dice} alt="" onClick={getAdvice} onKeyDown={getAdvice} />
+      </div>
+    </div>
+  );
+}
+
+export default AdviceCard;
