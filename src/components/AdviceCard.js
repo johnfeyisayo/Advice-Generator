303a4458af9d7c3cab03d54e37ca051ada4cd804
john feyisayo <johnfeyisayo@gmail.com>
2023-06-14 18:13:42 +0100

    update app

--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -5,25 +5,31 @@ import dividerMobile from "./images/pattern-divider-mobile.svg";
 import dice from "./images/icon-dice.svg";
 
 function AdviceCard() {
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState("");
 
-  const getAdvice = async() => {
-    const response = await axios.get('https://api.adviceslip.com/advice')
-    const advise = await response.data.slip
-    setAdvice(advise)
-  }
+  const getAdvice = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const advise = await response.data.slip;
+    setAdvice(advise);
+  };
 
   useEffect(() => {
     getAdvice();
-  },[])
-   return (
+  }, []);
+  return (
     <div className="card">
       <p>ADVICE #{advice.id}</p>
       <h2>{advice.advice}</h2>
       <img src={dividerDesktop} className="divider-desktop" alt="" />
       <img src={dividerMobile} className="divider-mobile" alt="" />
-      <div className="dice">
-        <img src={dice} alt="" onClick={getAdvice} onKeyDown={getAdvice} />
+      <div
+        className="dice"
+        role="button"
+        tabIndex={0}
+        onClick={getAdvice}
+        onKeyDown={getAdvice}
+      >
+        <img src={dice} alt="" />
       </div>
     </div>
   );
